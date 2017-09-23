3dbf12819d17834412233ac037badc0a38d48fa1
Raymond Wilfong <rwilfong@rewnet.com>
2017-09-23 11:06:04 -0500

    Created STM32Cube project with all files referenced instead of copied. Added basic CMake config files from github. Imported/Opened project from sources using CLion.

---
+++ cmake-build-debug/CMakeFiles/f1_blink.elf.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Users/rwilf/Dev/STM32/Whatcha/Src/main.c" "D:/Users/rwilf/Dev/STM32/Whatcha/cmake-build-debug/CMakeFiles/f1_blink.elf.dir/Src/main.c.obj"
+  "D:/Users/rwilf/Dev/STM32/Whatcha/Src/stm32f1xx_hal_msp.c" "D:/Users/rwilf/Dev/STM32/Whatcha/cmake-build-debug/CMakeFiles/f1_blink.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "D:/Users/rwilf/Dev/STM32/Whatcha/Src/stm32f1xx_it.c" "D:/Users/rwilf/Dev/STM32/Whatcha/cmake-build-debug/CMakeFiles/f1_blink.elf.dir/Src/stm32f1xx_it.c.obj"
+  "D:/Users/rwilf/Dev/STM32/Whatcha/Src/system_stm32f1xx.c" "D:/Users/rwilf/Dev/STM32/Whatcha/cmake-build-debug/CMakeFiles/f1_blink.elf.dir/Src/system_stm32f1xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103x6"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/Users/rwilf/Dev/STM32/Whatcha/cmake-build-debug/CMakeFiles/CMSIS.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
